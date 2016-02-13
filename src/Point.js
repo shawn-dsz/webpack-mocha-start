c56ede26d847f13048a72730cdfc8fbb8ce00833
dev-Shawn <shawn.dsouza@xero.com>
2016-02-13 21:37:33 +1300

    add eslint

--- src/Point.js
+++ src/Point.js
@@ -1,11 +1,11 @@
 class Point {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
+    this.x = x
+    this.y = y
   }
   toString() {
-    return `(${this.x},  dd ${this.y})`;
+    return `(${this.x},  dd ${this.y})`
   }
 }
 
-export default Point;
+export default Point
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
   entry: './src/app.js',
@@ -8,13 +8,23 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
+    preLoaders: [{
+      test: /\.js$/,
+      loaders: ['eslint-loader'],
+      exclude: /(node_modules|bower_components)/
+    }],
     loaders: [{
       loader: 'babel-loader',
       test: path.join(__dirname, 'src'),
       query: {
-        presets: 'es2015',
-      },
+        presets: 'es2015'
+      }
     }]
+  },
+  watch: true,
+  eslint: {
+    configFile: '.eslintrc',
+    quiet: false
   },
   plugins: [
     // Avoid publishing files when compilation fails
@@ -25,5 +35,5 @@ module.exports = {
     colors: true
   },
   // Create Sourcemaps for the bundle
-  devtool: 'source-map',
-};
+  devtool: 'source-map'
+}
